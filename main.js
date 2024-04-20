566cc34ce13ef32af0fcf26351043b3b2f091a6a
bayer2517 <kbairamov2517@gmail.com>
2024-04-20 18:19:25 -0500

    made weather condition part more dynamic

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Menu section
+
+
 document.querySelector("#open-nav-menu").addEventListener("click",function(){
     document.querySelector("header nav .wrapper").classList.add("nav-open");
 });
@@ -6,3 +9,15 @@ document.querySelector("#open-nav-menu").addEventListener("click",function(){
 document.querySelector("#close-nav-menu").addEventListener("click",function(){
     document.querySelector("header nav .wrapper").classList.remove("nav-open");
 });
+
+
+// Greeting section
+
+const greetingText = "Good Morning";
+const weatherCondition = "sunny";
+const userLocation = "Moscow";
+let tempature = 22.867;
+let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${tempature.toFixed()}°C outside.`;
+
+document.querySelector("#greeting").innerHTML = greetingText;
+document.querySelector("p#weather").innerHTML = weatherText;
