f52d6d3873aeddd68a1d2655ced86e1a8ec188b7
bayer2517 <kbairamov2517@gmail.com>
2024-04-21 21:30:20 -0500

    weather button conversion was created

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Menu section
 
+// Greeting section
 
 document.querySelector("#open-nav-menu").addEventListener("click",function(){
     document.querySelector("header nav .wrapper").classList.add("nav-open");
@@ -11,13 +12,40 @@ document.querySelector("#close-nav-menu").addEventListener("click",function(){
 });
 
 
-// Greeting section
-
 const greetingText = "Good Morning";
 const weatherCondition = "sunny";
 const userLocation = "Moscow";
 let tempature = 22.867;
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${tempature.toFixed()}°C outside.`;
+
 
 document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = weatherText;
+
+
+
+
+//WEATHER BUTTON
+
+let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(tempature).toFixed()}°F outside.`;
+let celsiumText = `The weather is ${weatherCondition} in ${userLocation} and it's ${tempature.toFixed()}°C outside.`;
+
+document.querySelector("p#weather").innerHTML = celsiumText;
+
+function celsiusToFahr(tempature) {
+    let fahr = (tempature * 9/5) + 32;
+    return fahr;
+}
+
+document.querySelector(".weather-group").addEventListener("click", function(event) { //execution conversion to fahr
+    
+    if (event.target.id == "celsius") { //celsium button
+        document.querySelector("p#weather").innerHTML = celsiumText;
+    } else if (event.target.id == "fahr") { //fahr button
+        document.querySelector("p#weather").innerHTML = fahrText;;
+    }
+});
+
+
+// NEXT
+
+
+
