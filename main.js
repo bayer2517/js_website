8dc5ab05feac8fa1a41933f74026f3275f5de128
bayer2517 <kbairamov2517@gmail.com>
2024-04-23 19:42:19 -0500

    clock section modified

--- main.js
+++ main.js
@@ -45,7 +45,12 @@ document.querySelector(".weather-group").addEventListener("click", function(even
 });
 
 
-// NEXT
+// Clock section 
 
 
-
+setInterval(function(){
+    let localTime = new Date();
+    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2,"0");
+    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
+    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
+},1000);
