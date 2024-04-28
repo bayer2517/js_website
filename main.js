4047cb79e67f452123efdce2c694573467cc602e
bayer2517 <kbairamov2517@gmail.com>
2024-04-28 14:10:56 -0500

    changed img from html code to js and created loop for future selection subnails and main image of the page

--- main.js
+++ main.js
@@ -54,3 +54,71 @@ setInterval(function(){
     document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
     document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
 },1000);
+
+
+
+
+// Gallery section 
+
+const galleryImages = [
+    {
+        src: "./assets/gallery/image1.jpg",
+        alt: "Thumbnail Image 1"
+    },
+        
+    {   
+        src: "./assets/gallery/image2.jpg",
+        alt: "Thumbnail Image 2",
+    },
+
+    {   
+        src: "./assets/gallery/image3.jpg",
+        alt: "Thumbnail Image 3"
+    }
+];
+
+/* for (let i in galleryImages) {
+    console.log(galleryImages[i])
+} */
+
+// Default image when page loaded amd thumbnail image
+let mainImage = document.querySelector('#gallery > img');
+let thumbnails = document.querySelector('#gallery > .thumbnails')
+
+mainImage.src = galleryImages[0].src;
+mainImage.alt = galleryImages[0].alt;
+
+// <img src="./assets/gallery/image1.jpg"
+// alt="Thumbnail Image 1" 
+// data-array-index="0" 
+// data-selected="true"></img>
+
+
+
+// Images selector 
+galleryImages.forEach(function(image,index){
+    let thumb = document.createElement("img");
+    thumb.src = image.src;
+    thumb.alt = image.alt
+    thumb.dataset.arrayIndex = index;
+    
+    //This is regulat id else condition for selecting first image 
+    //thumb.dataset.selected = false;
+    
+    /* if (index === 0){
+        thumb.dataset.selected = true;
+    }else {
+        thumb.dataset.selected = false
+    }
+    
+    thumbnails.appendChild(thumb); */
+
+    // or use ternary condition
+
+    thumb.dataset.selected = index === 0 ? true : false;  // condition ? do this : else do this
+    
+    thumbnails.appendChild(thumb);
+
+
+}); 
+
